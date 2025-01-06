534d10dece6a2f8408007e51a7f3cb50e5e0ad22
fromSoundToDev <jeunejuste@gmail.com>
2025-01-06 22:11:03 +0000

    testing with amazon

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ import puppeteer from 'puppeteer';
 
 (async()=>{
     const browser = await puppeteer.launch({
+        // permet de voir la lactiviter du script dans le navigateur afin de mieu debuger
         headless:false,
+        // aggrandi le navigateur au size de lecran 
         defaultViewport:false,
+        // dossir qui contien les donnee utilisateur 
         userDataDir: './myData'
     });
     const page = await browser.newPage();
-    await page.goto('https://www.cfetogo.tg/');
+    await page.goto('https://www.amazon.com/');
+
+    // parcourir le div parent qui contien linfo rechercher 
+    const handleProducts = await page.$$('.a-section .a-spacing-none .gw-card-layout')
+
+    for (const handleProduct of handleProducts) {
+
+        const productHeader = await page.evaluate(el=>el.querySelector("div>.a-cardui-header>h2").textContent,handleProduct);
+        console.log(productHeader);
+    }
 
 
-    
+
+
     // await page.screenshot({path:'cfetogo.png'})
     // await browser.close(); 
 })();
