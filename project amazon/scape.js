e887e5f3ae7910bf05b154cc802209d07a314557
fromSoundToDev <jeunejuste@gmail.com>
2025-02-21 10:42:53 +0000

    use of evaluate

--- project amazon/scape.js
+++ project amazon/scape.js
@@ -6,5 +6,10 @@ import puppeteer from "puppeteer";
     defaultViewport: null,
   });
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.fr/')
+  await page.goto('https://fr.wikipedia.org/wiki/Web_scraping')
+  const all_H2 = await page.evaluate(()=>{
+   return Array.from(document.querySelectorAll('h2')).map(el=>el.innerText)
+  });
+
+  console.log('titre des section :', all_H2);
 })()
